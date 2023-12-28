65b46d9c0e49d9bf690484bc31d619a271a68304
navin-git <nabinkharel142@gmail.com>
2023-12-28 17:14:37 +0545

    feat: dom in js

--- script/dom.js
+++ script/dom.js
@@ -1,1 +1,14 @@
-console.log("dsa");
+const search = document.getElementById("search");
+search.value = "tv";
+search.disabled = true;
+search.style.backgroundColor = "wheat";
+
+const cards = document.getElementsByClassName("card");
+// cards[0].style.backgroundColor = "gray";
+for (let i = 0; i < cards.length; i++) {
+  cards[i].style.backgroundColor = "gray";
+}
+// console.log(cards);
+
+const comp = document.querySelectorAll("div");
+console.log(comp);
